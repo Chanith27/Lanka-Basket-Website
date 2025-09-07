540fb0f706a8b9cbaeb21af55677491a0de1094d
Ashan Wickramanayaka <wmashan2@gmail.com>
2025-09-07 14:41:07 +0530

    Delete server directory

--- server/config/stripe.js
+++
@@ -1,7 +0,0 @@
-import stripe from 'stripe'
-import dotenv from 'dotenv'
-dotenv.config()
-
-const Stripe = process.env.STRIPE_SECRET_KEY ? stripe(process.env.STRIPE_SECRET_KEY) : null
-
-export default Stripe
